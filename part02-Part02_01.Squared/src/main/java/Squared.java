8cc8dd5b6379b67e200523d9c94d55640571d2bc
Kamran Ansari <crossthinking786@gmail.com>
2022-07-06 22:15:24 +0530

    initial commit

---
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class Squared {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int integer = Integer.valueOf(scanner.nextLine());
+        
+        System.out.print(integer*integer);
+    }
+}
