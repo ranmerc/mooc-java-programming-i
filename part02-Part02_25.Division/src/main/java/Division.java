d79078990e588d092b5c60f8ad5ad03d694516c4
Kamran Ansari <crossthinking786@gmail.com>
2022-07-08 13:15:59 +0530

    completed Part02_25.Division

--- part02-Part02_25.Division/src/main/java/Division.java
+++ part02-Part02_25.Division/src/main/java/Division.java
@@ -12,4 +12,9 @@ public class Division {
     }
 
     // implement the method here
+    public static void division(int numerator, int denominator) {
+        double result = (double) numerator / denominator;
+        
+        System.out.println(result);
+    }
 }
