9ad1bffae1a99e70fd3e719d6c926ea18608d1ba
Kamran Ansari <crossthinking786@gmail.com>
2022-07-08 09:34:10 +0530

    completed Part02_16.FromWhereToWhere

--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -7,5 +7,20 @@ public class FromWhereToWhere {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Where to?");
+        int toNum = Integer.valueOf(scanner.nextLine());
+        
+//        for (int i = 1; i <= toNum; i++) {
+//            System.out.println(i);
+//        }
+        
+        System.out.println("Where from?");
+        int fromNum = Integer.valueOf(scanner.nextLine());
+        
+        if (toNum >= fromNum) {
+            for (int i = fromNum; i <= toNum; i++) {
+                System.out.println(i);
+            }
+        }
     }
 }
