769f9459df7e436af48043cd5cb53db037623914
Kamran Ansari <crossthinking786@gmail.com>
2022-07-08 10:40:31 +0530

    completed Part02_20.RepeatingBreakingAndRemembering

--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -13,6 +13,36 @@ public class RepeatingBreakingAndRemembering {
         // the parts you haven't done, but you'll get points for the finished parts.
         
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Give numbers:");
+        
+        int sum = 0;
+        int numberOfNumbers = 0;
+        int evenNumbers = 0;
+        int oddNumbers = 0;
+        
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == -1) {
+                break;
+            }
+            
+            if (number % 2 == 0) {
+                evenNumbers++;
+            } else {
+                oddNumbers++;
+            }
+            
+            sum += number;
+            numberOfNumbers++;
+        }
 
+        System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + numberOfNumbers);
+        System.out.println("Average: " + ((double) sum / numberOfNumbers));
+        System.out.println("Even: " + evenNumbers);
+        System.out.println("Odd: " + oddNumbers);
     }
 }
