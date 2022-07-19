1a766275542e650176d060828e284b341f7812f9
Kamran Ansari <crossthinking786@gmail.com>
2022-07-19 23:57:25 +0530

    completed Part03_17.RemoveLast

--- part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
+++ part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
@@ -6,5 +6,14 @@ public class RemoveLast {
     public static void main(String[] args) {
         // Try your method in here
     }
-
+    
+    public static void removeLast(ArrayList<String> strings) {
+        int listSize = strings.size();
+        
+        if (listSize == 0) {
+            return;
+        }
+        
+        strings.remove(listSize - 1);
+    }
 }
