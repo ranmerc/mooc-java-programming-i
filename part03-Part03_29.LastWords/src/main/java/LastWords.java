a4fb792eb6e9c950bfa8fa2a34171775ac2a9eb3
Kamran Ansari <crossthinking786@gmail.com>
2022-07-20 23:13:40 +0530

    completed Part03_29.LastWords

--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -5,7 +5,18 @@ public class LastWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] words = input.split(" ");
+            
+            System.out.println(words[words.length - 1]);
+        }
 
     }
 }
