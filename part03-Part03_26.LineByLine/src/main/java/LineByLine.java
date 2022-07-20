1a6c8c4f0ae5f249efdbe8594be3d067b30b7fc4
Kamran Ansari <crossthinking786@gmail.com>
2022-07-20 23:05:00 +0530

    completed Part03_26.LineByLine

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -6,6 +6,18 @@ public class LineByLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] words = input.split(" ");
+            
+            for (String word: words) {
+                System.out.println(word);
+            }
+        }
     }
 }
