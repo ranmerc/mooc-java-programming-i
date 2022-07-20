af40b94634b999ab6e8dd84ba951ff4bb9aa45a3
Kamran Ansari <crossthinking786@gmail.com>
2022-07-20 23:31:00 +0530

    completed Part03_32.PersonalDetails

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,34 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        int maxNameLength = 0;
+        String longestName = "";
+        int sumOfYears = 0;
+        int recordCount = 0;
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] fields = input.split(",");
+            
+            int nameLength = fields[0].length();
+            
+            if (nameLength > maxNameLength) {
+                maxNameLength = nameLength;
+                longestName = fields[0];
+            }
+            
+            int year = Integer.valueOf(fields[1]);
+            
+            sumOfYears += year;
+            recordCount += 1;
+        }
+        
+        System.out.println("Longest name: " + longestName);
+        System.out.println("Average of the birth years: " + ((double) sumOfYears / recordCount));
     }
 }
