85fb5cfa2f8dae139139746ca59b0fc25fa20d84
Kamran Ansari <crossthinking786@gmail.com>
2022-07-20 23:20:55 +0530

    completed Part03_30.AgeOfTheOldest

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -5,7 +5,24 @@ public class AgeOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        
+        int maxAge = 0;
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] fields = input.split(",");
+            
+            int age = Integer.valueOf(fields[1]);
+            if (age > maxAge) {
+                maxAge = age;
+            }
+        }
+        
+        System.out.println("Age of the oldest: " + maxAge);
     }
 }
