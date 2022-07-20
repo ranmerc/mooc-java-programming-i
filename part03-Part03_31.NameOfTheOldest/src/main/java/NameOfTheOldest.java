1886bccbd54a8178225b181aaed8d1cb5d553068
Kamran Ansari <crossthinking786@gmail.com>
2022-07-20 23:24:00 +0530

    completed Part03_31.NameOfTheOldest

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -6,6 +6,25 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        int maxAge = 0;
+        String oldestName = "";
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] fields = input.split(",");
+            
+            int age = Integer.valueOf(fields[1]);
+            if (age > maxAge) {
+                maxAge = age;
+                oldestName = fields[0];
+            }
+        }
+        
+        System.out.println("Name of the oldest: " + oldestName);
     }
 }
