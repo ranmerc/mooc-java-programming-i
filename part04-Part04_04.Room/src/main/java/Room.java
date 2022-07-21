dcfc515a677ee61dee6723775332cc2409bf489a
Kamran Ansari <crossthinking786@gmail.com>
2022-07-21 18:15:02 +0530

    completed Part04_04.Room

---
+++ part04-Part04_04.Room/src/main/java/Room.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author ranmerc
+ */
+public class Room {
+    private String code;
+    private int seats;
+    
+    public Room(String classCode, int noOfSeats) {
+        this.code = classCode;
+        this.seats = noOfSeats;
+    }
+}
