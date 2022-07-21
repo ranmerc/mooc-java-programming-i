a272a50ee10a1cec13ea65552aab0c2dad4bb4bd
Kamran Ansari <crossthinking786@gmail.com>
2022-07-21 17:39:46 +0530

    completed Part04_01.YourFirstAccount

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,9 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account myAccount = new Account("Kamran", 100);
+        myAccount.deposit(20);
+        
+        System.out.println(myAccount);
     }
 }
