a3a4f29ddde8feaefb653d7595081d9a2e1ec472
Kamran Ansari <crossthinking786@gmail.com>
2022-07-21 20:35:56 +0530

    completed Part04_14.Multiplier

---
+++ part04-Part04_14.Multiplier/src/main/java/Multiplier.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ * @author ranmerc
+ */
+public class Multiplier {
+    private int value;
+    
+    public Multiplier(int number) {
+        this.value = number;
+    }
+    
+    public int multiply(int number) {
+        return this.value * number;
+    }
+}
