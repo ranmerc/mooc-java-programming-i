a59cca271cdbadcd6ad822a156cbd0bac3a5c105
Kamran Ansari <crossthinking786@gmail.com>
2022-07-21 20:03:44 +0530

    completed Part04_10.Song

---
+++ part04-Part04_10.Song/src/main/java/Song.java
@@ -0,0 +1,22 @@
+
+/**
+ *
+ * @author ranmerc
+ */
+public class Song {
+    private String name;
+    private int seconds;
+    
+    public Song(String name, int length) {
+        this.name = name;
+        this.seconds = length;
+    }
+    
+    public String name() {
+        return this.name;
+    }
+    
+    public int length() {
+        return this.seconds;
+    }
+}
