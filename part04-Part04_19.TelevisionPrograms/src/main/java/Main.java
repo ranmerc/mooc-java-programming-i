23470002f61f4bc3aac082ac26e660034517334f
Kamran Ansari <crossthinking786@gmail.com>
2022-07-22 16:13:31 +0530

    completed Part04_19.TelevisionPrograms

--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -8,6 +8,30 @@ public class Main {
 
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
+        
+        while (true) {
+            System.out.println("Name:");
+            String showName = scanner.nextLine();
+            
+            if (showName.isEmpty()) {
+                break;
+            }
+            
+            System.out.println("Duration:");
+            int showDuration = Integer.valueOf(scanner.nextLine());
+            
+            TelevisionProgram show = new TelevisionProgram(showName, showDuration);
+            
+            programs.add(show);
+        }
+        
+        System.out.println("Program's maximum duration?");
+        int maxDuration = Integer.valueOf(scanner.nextLine());
+        
+        for (TelevisionProgram show: programs) {
+            if (show.getDuration() <= maxDuration) {
+                System.out.println(show);
+            }
+        }
     }
 }
