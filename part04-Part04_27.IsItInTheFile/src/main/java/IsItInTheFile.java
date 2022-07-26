f51d520d9a5b40647cb7bd23833334be29f27f2e
Kamran Ansari <crossthinking786@gmail.com>
2022-07-26 21:09:01 +0530

    completed Part04_27.IsItInTheFile

--- part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -12,6 +12,20 @@ public class IsItInTheFile {
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
-
+        
+        try (Scanner fileScan = new Scanner(Paths.get(file))) {
+            while(fileScan.hasNextLine()) {
+                String name = fileScan.nextLine();
+                
+                if(name.equals(searchedFor)) {
+                    System.out.println("Found!");
+                    return;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + "failed.");
+        }
+        
+        System.out.println("Not found.");
     }
 }
