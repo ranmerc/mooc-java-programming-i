4e3789471faefb86556ca45b8765b1512f50e2fe
Kamran Ansari <crossthinking786@gmail.com>
2022-07-26 20:58:35 +0530

    completed Part04_25.PrintingASpecifiedFile

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -7,5 +7,16 @@ public class PrintingASpecifiedFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Which file should have its contents printed?");
+        String fileName = scanner.nextLine();
+
+        try (Scanner fileScan = new Scanner(Paths.get(fileName))) {
+            while (fileScan.hasNextLine()) {
+                String line = fileScan.nextLine();
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
