4a20914ba5e6355d6ac6ac6cafff1f1c9631bdca
Kamran Ansari <crossthinking786@gmail.com>
2022-07-27 17:43:44 +0530

    completed Part04_31.SportStatistics

--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -8,6 +8,49 @@ public class SportStatistics {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("File:");
+        String fileName = scan.nextLine();
+
+        System.out.println("Team:");
+        String teamName = scan.nextLine();
+
+        int gameCount = 0;
+        int winCount = 0;
+        int lossCount = 0;
+
+        try (Scanner fileScan = new Scanner(Paths.get(fileName))) {
+            while (fileScan.hasNextLine()) {
+                String line = fileScan.nextLine();
+
+                String[] fields = line.split(",");
+                String homeTeamName = fields[0];
+                String visitingTeamName = fields[1];
+                int homeTeamPoints = Integer.valueOf(fields[2]);
+                int visitingTeamPoints = Integer.valueOf(fields[3]);
+
+                if (homeTeamName.equals(teamName)) {
+                    gameCount++;
+                    if (homeTeamPoints > visitingTeamPoints) {
+                        winCount++;
+                    } else {
+                        lossCount++;
+                    }
+                } else if (visitingTeamName.equals(teamName)) {
+                    gameCount++;
+                    if (homeTeamPoints < visitingTeamPoints) {
+                        winCount++;
+                    } else {
+                        lossCount++;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        System.out.println("Games: " + gameCount);
+        System.out.println("Wins: " + winCount);
+        System.out.println("Losses: " + lossCount);
     }
 
 }
