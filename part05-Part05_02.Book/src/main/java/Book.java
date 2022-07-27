02f0a8b2c987eb579a046b9784a330fbf735b4b5
Kamran Ansari <crossthinking786@gmail.com>
2022-07-27 18:34:04 +0530

    completed Part05_02.Book

---
+++ part05-Part05_02.Book/src/main/java/Book.java
@@ -0,0 +1,33 @@
+
+/**
+ *
+ * @author ranmerc
+ */
+public class Book {
+    
+    private String author;
+    private String title;
+    private int pageCount;
+    
+    public Book(String author, String name, int pages) {
+        this.author = author;
+        this.title = name;
+        this.pageCount = pages;
+    }
+    
+    public String getAuthor() {
+        return this.author;
+    }
+    
+    public String getName() {
+        return this.title;
+    }
+    
+    public int getPages() {
+        return this.pageCount;
+    }
+    
+    public String toString() {
+        return this.author + ", " + this.title + ", " + this.pageCount + " pages";
+    }
+}
