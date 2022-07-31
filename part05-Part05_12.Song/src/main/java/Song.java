afca90065735db66769a6698abfbe56fb43cba60
Kamran Ansari <crossthinking786@gmail.com>
2022-07-31 18:55:59 +0530

    completed Part05_12.Song

--- part05-Part05_12.Song/src/main/java/Song.java
+++ part05-Part05_12.Song/src/main/java/Song.java
@@ -10,6 +10,27 @@ public class Song {
         this.name = name;
         this.durationInSeconds = durationInSeconds;
     }
+    
+    public boolean equals(Object compared) {
+        // check if same reference
+        if (this == compared) {
+            return true;
+        }
+        
+        // check if same type of object
+        if (!(compared instanceof Song)) {
+            return false;
+        }
+        
+        // cast for comparison
+        Song comparedSong = (Song) compared;
+        
+        if(comparedSong.name.equals(this.name) && comparedSong.artist.equals(this.artist) && comparedSong.durationInSeconds == this.durationInSeconds) {
+            return true;
+        }
+        
+        return false;
+    }
 
     @Override
     public String toString() {
