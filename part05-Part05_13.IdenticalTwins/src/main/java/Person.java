0f1673e389fe58b67cfb303dd8284d4746eab568
Kamran Ansari <crossthinking786@gmail.com>
2022-07-31 19:01:16 +0530

    completed Part05_13.IdenticalTwins

--- part05-Part05_13.IdenticalTwins/src/main/java/Person.java
+++ part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -16,4 +16,21 @@ public class Person {
     }
 
     // implement an equals method here for checking the equality of objects
+    public boolean equals(Object compared) {
+        if (this == compared) {
+            return true;
+        }
+        
+        if (!(compared instanceof Person)) {
+            return false;
+        }
+        
+        Person comparedPerson = (Person) compared;
+        
+        if (comparedPerson.name.equals(this.name) && comparedPerson.height == this.height && comparedPerson.weight == this.weight && comparedPerson.birthday.equals(this.birthday)) {
+            return true;
+        }
+        
+        return false;
+    }
 }
