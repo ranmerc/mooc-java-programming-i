543bf78500b8ccb7e8fb870c34f3ba17570f8e99
Kamran Ansari <crossthinking786@gmail.com>
2022-07-31 18:39:16 +0530

    completed Part05_11.ComparingApartments

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -11,4 +11,27 @@ public class Apartment {
         this.princePerSquare = pricePerSquare;
     }
 
+    public boolean largerThan(Apartment compared) {
+        if (this.squares > compared.squares) {
+            return true;
+        }
+        return false;
+    }
+    
+    public int priceDifference(Apartment compared) {
+        int ownPrice = this.princePerSquare * this.squares;
+        int comparedPrice = compared.princePerSquare * compared.squares;
+        
+        return Math.abs(ownPrice - comparedPrice);
+    }
+    
+    public boolean moreExpensiveThan(Apartment compared) {
+        int ownPrice = this.princePerSquare * this.squares;
+        int comparedPrice = compared.princePerSquare * compared.squares;
+        
+        if (ownPrice > comparedPrice) {
+            return true;
+        }
+        return false;
+    }
 }
