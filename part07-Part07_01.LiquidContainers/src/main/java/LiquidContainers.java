dc0ddda374f4dd17ba871a353b5322754ca0dca3
Kamran Ansari <crossthinking786@gmail.com>
2022-08-03 17:44:48 +0530

    completed Part07_01.LiquidContainers

--- part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
+++ part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,16 +5,61 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int firstContainer = 0;
+        int secondContainer = 0;
 
         while (true) {
+            System.out.println("First: " + firstContainer + "/100");
+            System.out.println("Second: " + secondContainer + "/100");
+            
             System.out.print("> ");
 
             String input = scan.nextLine();
+
             if (input.equals("quit")) {
                 break;
             }
+            
+            String[] parts = input.split(" ");
 
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+            
+            if (amount < 0) {
+                continue;
+            }
+            
+            if (command.equals("add")) {
+                firstContainer += amount;
+                
+                if (firstContainer > 100) {
+                    firstContainer = 100;
+                }
+            }
+            
+            if (command.equals("move")) {
+                if (amount >= firstContainer) {
+                    secondContainer += firstContainer;
+                    firstContainer = 0;
+                } else {
+                    secondContainer += amount;
+                    firstContainer -= amount;
+                }
+                
+                if (secondContainer > 100) {
+                    secondContainer = 100;
+                }
+                
+                continue;
+            }
+            
+            if (command.equals("remove")) {
+                if (amount >= secondContainer) {
+                    secondContainer = 0;
+                } else {
+                    secondContainer -= amount;
+                }
+            }
         }
     }
 
