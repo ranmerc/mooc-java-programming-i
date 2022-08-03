8d6a72e88876407a6bff7a1998f6cfee70c6b3b9
Kamran Ansari <crossthinking786@gmail.com>
2022-08-03 16:41:23 +0530

    completed Part06_13.Exercises

--- part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,43 @@
 
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import org.junit.Test;
+import org.junit.Before;
 
 public class ExerciseManagementTest {
 
+    private ExerciseManagement management;
+
+    @Before
+    public void intialize() {
+        management = new ExerciseManagement();
+    }
+
+    @Test
+    public void exerciseListEmptyAtBeginning() {
+        ExerciseManagement management = new ExerciseManagement();
+        assertEquals(0, management.exerciseList().size());
+    }
+
+    @Test
+    public void addingExerciseGrowsListByOne() {
+        ExerciseManagement management = new ExerciseManagement();
+        management.add("exercise");
+        assertTrue(management.exerciseList().contains("exercise"));
+    }
+
+    @Test
+    public void exerciseCanBeMarkedAsCompleted() {
+        management.add("New exercise");
+        management.markAsCompleted("New exercise");
+        assertTrue(management.isCompleted("New exercise"));
+    }
+
+    @Test
+    public void ifNotMarkedCompletedIsNotCompleted() {
+        management.add("New exercise");
+        management.markAsCompleted("New exercise");
+        assertFalse(management.isCompleted("Some exercise"));
+    }
 }
