b0c3425e0849c490aa8e96f84911aaa7c8a61419
Kamran Ansari <crossthinking786@gmail.com>
2022-08-04 15:18:02 +0530

    completed Part07_03.Sorting

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -6,4 +6,45 @@ public class MainProgram {
         int[] array = {3, 1, 5, 99, 3, 12};
     }
 
+    public static int smallest(int[] array) {
+        int smallest = array[0];
+        
+        for (int element : array) {
+            if (element < smallest) {
+                smallest = element;
+            }
+        }
+        
+        return smallest;
+    }
+    
+    public static int indexOfSmallest(int[] array) {
+        return indexOfSmallestFrom(array, 0);
+    }
+    
+    public static int indexOfSmallestFrom(int[] table, int startIndex) {
+        int smallestIndex = startIndex;
+        
+        for (int i = startIndex; i < table.length; i++) {
+            if (table[i] < table[smallestIndex]) {
+                smallestIndex = i;
+            }
+        }
+        
+        return smallestIndex;
+    }
+    
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1]= array[index2];
+        array[index2] = temp;
+    }
+    
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array);
+            
+            swap(array, i, indexOfSmallestFrom(array, i));
+        }
+    }
 }
